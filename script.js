6356967178f6bccbf7be53df7a982f76c68d786a
Swarup-git-hub <hemaswarupbande5@gmail.com>
2025-06-29 21:25:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const nav = document.querySelector('.nav-bar');
+// const navlayer = document.getElementById('layer');
+const link1 = document.getElementById('link1');
+const link2 = document.getElementById('link2');
+const link3 = document.getElementById('link3');
+const link4 = document.getElementById('link4');
+const link5 = document.getElementById('link5');
+const favicon = document.getElementById('favicon');
+
+  function updateFavicon() {
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    favicon.href = isDark ? "profile-light.png" : "profile-darkk.png";
+  }
+  // Initially call on load
+  updateFavicon();
+
+  // Listen for system theme changes
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateFavicon);
+
+  //reload to home page
+  setTimeout(()=>{
+      window.location="#home";
+      
+  },1000);
+
+function open_menu(){
+    nav.classList.add('show');
+    // navlayer.classList.add('sheet');
+
+}
+
+function close_menu(){
+    nav.classList.remove('show')
+    // navlayer.classList.remove('sheet');
+
+}
+
+link1.onclick=close_menu;//assigning the function
+link2.onclick=close_menu;
+link3.onclick=close_menu;
+link4.onclick=close_menu;
+link5.onclick=close_menu;
